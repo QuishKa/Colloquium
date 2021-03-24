81349dc6620b3bb4827dd1288800ebd9444798a2
Viacheslav <31708699+QuishKa@users.noreply.github.com>
2021-03-25 02:31:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ import Rational
 import Polynomial
 
 # Main code and all the tests go here
-a = int(input())
-b = int(input())
-print(Integer.Test.ssum(a, b))
+num = input()
+count = 0
+int_array = []
+dec_array = []
+while num != 'exit':
+    int_array.append(1)
+    pos = len(num)
+    for i in range(len(num)):
+        if num[i] == '.':
+            pos = i
+            break
+        if num[i] == '-':
+            int_array[0] *= -1
+        else:
+            int_array.append(num[i])
+            count += 1
+    int_array[0] *= count
+    count = 0
+    dec_array.append(0)
+    for i in range(pos + 1, len(num)):
+        dec_array.append(num[i])
+        count += 1
+    dec_array[0] = count
+    print(*int_array)
+    print(*dec_array)
+    num = input()
+    int_array.clear()
+    dec_array.clear()
+    count = 0
