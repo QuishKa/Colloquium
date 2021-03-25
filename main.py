e0a98aea62bf2a6b1f6ce3c98902a28e62ccdfb7
Viacheslav <31708699+QuishKa@users.noreply.github.com>
2021-03-25 13:58:43 +0300

    getint

--- main.py
+++ main.py
@@ -4,29 +4,8 @@ import Natural
 import Integer
 import Rational
 import Polynomial
-import random
 
 
 # Main code and all the tests go here
-def get_int():
-    if random.randint(0, 100) < 10:
-        num = str(random.randint(0, 2))
-    else:
-        num = str(random.randint(0, 99 ** random.randint(1, 5000)))
-    count = 0
-    length = 1
-    int_array = [length, []]
-    for i in range(len(num)):
-        if num[i] == '-':
-            int_array[0] *= -1
-        else:
-            int_array[1].append(num[i])
-            count += 1
-    int_array[0] *= count
-    print(int_array[0])
-    print(*int_array[1])
-    return int_array
-
-
 for i in range(100):
-    get_int()
+    Integer.get_int()
