0ee72591157a914a5b47e2a3b0b119d29500b488
unknown <vsradabolsky@gmail.com>
2021-04-01 23:05:57 +0300

    create TRANS_N_Z and TRANS_Z_N functions

---
+++ Integer/TRANS_N_Z.py
@@ -0,0 +1,6 @@
+#Author: Radabolsky V.S. 0308
+
+
+def TRANS_N_Z(natural):
+    integer = [0, natural[0], natural[1].copy()]
+    return integer
---
+++ Integer/TRANS_Z_N.py
@@ -0,0 +1,6 @@
+#Author Radabolsky V.S. 0308
+
+def TRANS_Z_N(integer):
+    a = integer.copy()
+    natural = [a[1], a[2].copy()]
+    return natural
