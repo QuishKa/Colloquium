b81c2e0365b185121642ba8162b258517a302ec3
Magicflora <74364802+Magicflora@users.noreply.github.com>
2021-04-05 21:41:49 +0300

    пофиксил ADD_ZZ_Z

--- Baev/ADD_ZZ_Z.py
+++ Baev/ADD_ZZ_Z.py
@@ -1,4 +1,8 @@
 def ADD_ZZ_Z(num1z, num2z):
+    num1n = ABS_Z_N(num1z)
+    num2n = ABS_Z_N(num2z)
+    resn = [1, [0]]
+    resz = [0, 1, [0]]
     # a = 0, b = 0 => a + b = 0
     if POZ_Z_D(num1z) == 0 and POZ_Z_D(num2z) == 0:
         return [0, 1, [0]]
@@ -11,28 +15,34 @@ def ADD_ZZ_Z(num1z, num2z):
 
     # a > 0, b > 0 => a + b = |a| + |b|
     if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 2:
-        return [0, ADD_NN_N(num1z, num2z)[0], ADD_NN_N(num1z, num2z)[1]]
+        resn = ADD_NN_N(num1n, num2n)
+        return [0, resn[0], resn[1]]
 
     # a > 0, b < 0, |a| > |b| => a + b = |a| - |b|
-    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 2:
-        return [0, SUB_NN_N(ABS_Z_N(num1z), ABS_Z_N(num2z))[0], SUB_NN_N(ABS_Z_N(num1z), ABS_Z_N(num2z))[1]]
+    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(num1n, num2n) == 2:
+        resn = SUB_NN_N(num1n, num2n)
+        return [0, resn[0], resn[1]]
     # a > 0, b < 0, |a| < |b| => a + b = -1 * (|b| - |a|)
-    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 1:
-        return [1, SUB_NN_N(ABS_Z_N(num2z), ABS_Z_N(num1z))[0], SUB_NN_N(ABS_Z_N(num2z), ABS_Z_N(num1z))[1]]
+    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(num1n, num2n) == 1:
+        resn = SUB_NN_N(num2n, num1n)
+        return [1, resn[0], resn[1]]
     # a > 0, b < 0, |a| = |b| => a + b = 0
-    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 0:
+    if POZ_Z_D(num1z) == 2 and POZ_Z_D(num2z) == 1 and COM_NN_D(num1n, num2n) == 0:
         return [0, 1, [0]]
 
     # a < 0, b > 0, |a| > |b| => a + b = -1 * (|a| - |b|)
-    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 2:
-        return [1, SUB_NN_N(ABS_Z_N(num1z), ABS_Z_N(num2z))[0], SUB_NN_N(ABS_Z_N(num1z), ABS_Z_N(num2z))[1]]
+    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(num1n, num2n) == 2:
+        resn = SUB_NN_N(num1n, num2n)
+        return [1, resn[0], resn[1]]
     # a < 0, b > 0, |a| < |b| => a + b = |b| - |a|
-    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 1:
-        return [0, SUB_NN_N(ABS_Z_N(num2z), ABS_Z_N(num1z))[0], SUB_NN_N(ABS_Z_N(num2z), ABS_Z_N(num1z))[1]]
+    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(num1n, num2n) == 1:
+        resn = SUB_NN_N(num2n, num1n)
+        return [0, resn[0], resn[1]]
     # a < 0, b > 0, |a| = |b| => a + b = 0
-    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(ABS_Z_N(num1z), ABS_Z_N(num2z)) == 0:
+    if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 2 and COM_NN_D(num1n, num2n) == 0:
         return [0, 1, [0]]
 
     # a < 0, b < 0 => a + b = -1 * (|a| + |b|)
     if POZ_Z_D(num1z) == 1 and POZ_Z_D(num2z) == 1:
-        return [1, ADD_NN_N(num1z, num2z)[0], ADD_NN_N(num1z, num2z)[1]]
+        resn = ADD_NN_N(num1n, num2n)
+        return [1, resn[0], resn[1]]
