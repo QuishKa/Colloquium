d4407f11787a13ff253353398b05f6d89d0be86d
Sno0oker <eltsov-vitaliy@mail.ru>
2021-04-05 19:11:07 +0300

    Update ADD_NN_N.py
    Исправил косяк со счетом

--- Natural/ADD_NN_N.py
+++ Natural/ADD_NN_N.py
@@ -6,21 +6,26 @@ def ADD_NN_N(arr1, arr2):
     arr1[1].reverse()
     arr2[1].reverse()
     shift = 0
+    s = 0
     if big_num == 1:
         for i in range(arr1[0]):
             sum = arr1[1][i] + arr2[1][i] + shift
             arr2[1][i] = sum % 10
             shift = sum // 10
+            s += 1
         if shift != 0:
             if arr1[0] == arr2[0]:
                 arr2[1].insert(arr1[0], shift)
                 arr2[0] += 1
             else:
-                arr2[1][arr1[0]] += shift
-                if arr2[1][arr1[0]] == 10:
-                    arr2[1][arr1[0]] = 0
-                    arr2[1].insert(arr1[0]+1, shift)
-                    arr2[0] += 1
+                k = 0
+                while arr2[1][arr1[0]+k] == 9:
+                    arr2[1][arr1[0]+k] = 0
+                    if (k + s + 1) >= arr2[0]:
+                      arr2[1].insert(arr1[0]+1+k, 0)
+                      arr2[0] += 1
+                    k += 1
+                arr2[1][arr1[0]+k] += 1
         arr1[1].reverse()
         arr2[1].reverse()
         return arr2
@@ -29,16 +34,20 @@ def ADD_NN_N(arr1, arr2):
             sum = arr1[1][i] + arr2[1][i] + shift
             arr1[1][i] = sum % 10
             shift = sum // 10
+            s += 1
         if shift != 0:
             if arr1[0] == arr2[0]:
                 arr1[1].insert(arr2[0], shift)
                 arr1[0] += 1
             else:
-                arr1[1][arr2[0]] += shift
-                if arr1[1][arr2[0]] == 10:
-                    arr1[1][arr2[0]] = 0
-                    arr1[1].insert(arr2[0]+1, shift)
-                    arr1[0] += 1
+                k = 0
+                while arr1[1][arr2[0] + k] == 9:
+                    arr1[1][arr2[0] + k] = 0
+                    if (k + s + 1) >= arr1[0]:
+                        arr1[1].insert(arr2[0] + 1 + k, 0)
+                        arr1[0] += 1
+                    k += 1
+                arr1[1][arr2[0] + k] += 1
         arr1[1].reverse()
         arr2[1].reverse()
         return arr1
