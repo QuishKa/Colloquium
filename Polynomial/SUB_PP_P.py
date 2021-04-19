85e672897b7486e14c2b5680e58032863d9bff38
Sno0oker <eltsov-vitaliy@mail.ru>
2021-04-19 23:40:32 +0300

    SUB_PP_P
    Фикс с нулями

--- Polynomial/SUB_PP_P.py
+++ Polynomial/SUB_PP_P.py
@@ -1,32 +1,23 @@
 from Rational import SUB_QQ_Q
-from Rational import RED_Q_Q
 
 
 def SUB_PP_P(arr1, arr2):
     if arr1[0] > arr2[0]:
-        r = arr2[0]
         for i in range(arr2[0] + 1):
             arr1[1][i] = SUB_QQ_Q(arr1[1][i], arr2[1][i])
-        i = 0
-        while i != r + 1:
-            if arr1[1][i][0][2] == [0]:
-                arr1[1].remove(arr1[1][i])
-                arr1[0] -= arr1[0]
-                r = r - 1
-                i -= 1
-            i += 1
-            return arr1
+        i = arr2[0]
+        while arr1[1][i][0][2] == [0] and i > 0:
+            arr1[1].pop()
+            arr1[0] = arr1[0] - 1
+            i = i - 1
+        return arr1
     else:
-        r = arr1[0]
         for i in range(arr1[0]+1):
             arr2[1][i] = SUB_QQ_Q(arr1[1][i], arr2[1][i])
-        i = 0
-        while i != r + 1:
-            if arr2[1][i][0][2] == [0]:
-                arr2[1].remove(arr2[1][i])
-                arr2[0] -= arr2[0]
-                r = r - 1
-                i -= 1
-            i += 1
+        i = arr1[0]
+        while arr2[1][i][0][2] == [0] and i > 0:
+            arr2[1].pop()
+            arr2[0] = arr2[0] - 1
+            i = i - 1
         return arr2
 #Ельцов В.
