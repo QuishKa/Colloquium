2de4a9e3b47cdffb28b324bc145b10a3c8ff9c83
unknown <vsradabolsky@gmail.com>
2021-04-23 00:49:55 +0300

    final fix der

--- Polynomial/DER_P_P.py
+++ Polynomial/DER_P_P.py
@@ -7,7 +7,7 @@ from DIV_PP_P import copy_polynom
 
 def DER_P_P(polynom):
     p = copy_polynom(polynom)
-    for i in range(1, p[0]):
+    for i in range(1, p[0] + 1):
         zi = list(map(int, str(i)))
         zi = [[0, len(zi), zi], [1, [1]]]
         p[1][i - 1] = MUL_QQ_Q(p[1][i], zi)
