d669db6596c26cb5ce7c1d7d4b55c40e95d4afc2
unknown <vsradabolsky@gmail.com>
2021-04-23 00:38:44 +0300

    fix DER_P_P and MUL_QQ_Q

--- Polynomial/DER_P_P.py
+++ Polynomial/DER_P_P.py
@@ -1,3 +1,5 @@
+# Author: Radabolsky V.S. 0308
+
 from Rational import MUL_QQ_Q
 from DIV_PP_P import copy_polynom
 
@@ -5,10 +7,10 @@ from DIV_PP_P import copy_polynom
 
 def DER_P_P(polynom):
     p = copy_polynom(polynom)
-    for i in range(p[0] - 1):
+    for i in range(1, p[0]):
         zi = list(map(int, str(i)))
         zi = [[0, len(zi), zi], [1, [1]]]
-        p[1][i] = MUL_QQ_Q(p[1][i + 1], zi)
+        p[1][i - 1] = MUL_QQ_Q(p[1][i], zi)
     p[1].pop()
     p[0] -= 1
     return p
--- Rational/MUL_QQ_Q.py
+++ Rational/MUL_QQ_Q.py
@@ -1,6 +1,6 @@
 #Author: Radabolsky Vladislav 0308
 
-from Natural import new_MUL_NN_N
+from Natural import MUL_NN_N
 from RED_Q_Q import RED_Q_Q
 
 def MUL_ZZ_Z(arr1, arr2):
@@ -8,7 +8,7 @@ def MUL_ZZ_Z(arr1, arr2):
     ar1 = [arr1[1], list(arr1[2])]
     ar2 = [arr2[1], list(arr2[2])]
 
-    res = new_MUL_NN_N(ar1, ar2)
+    res = MUL_NN_N(ar1, ar2)
 
     if res[1] == [0]:
         return [0, 1, [0]]
@@ -20,7 +20,7 @@ def MUL_ZZ_Z(arr1, arr2):
 
 
 def MUL_QQ_Q(first, second):
-    result = list([MUL_ZZ_Z(first[0], second[0]), new_MUL_NN_N(first[1], second[1])])
+    result = list([MUL_ZZ_Z(first[0], second[0]), MUL_NN_N(first[1], second[1])])
     return RED_Q_Q(result)
 
 
